cb5d11f0c3fc0665b8b913a5c5253b9370b37c19
tzitziris <stzitzires@gmail.com>
2025-08-09 15:21:53 +0300

    first commit

---
+++ src/main/java/com/springboot/wheatherApi/WheatherApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.wheatherApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheatherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WheatherApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/wheatherApi/WheatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.wheatherApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheatherApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
